4311f7117acc5e502f83ba956b7a197512e9a95f
Konstantin Merenkov <kmerenkov@gmail.com>
2008-09-29 00:08:28 +0400

    initial commit

---
+++ clitter.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2008, Konstantin Merenkov <kmerenkov@gmail.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY Konstantin Merenkov <kmerenkov@gmail.com> ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import sys
+import twitter
+from ConfigParser import RawConfigParser
+import os
+from pprint import pprint
+
+
+usage = \
+"""Usage:
+      a <status>    - update your status
+      d <id>        - destroy status by id
+      f [user_id]   - fetch user statuses by user_id
+                      (if user_id is ommiitted, fetch your own statuses)"""
+
+class Clitter(object):
+    def __init__(self):
+        self.verbose = False
+        self.command = None
+        self.credentials = None
+
+    def read_config(self):
+        config = RawConfigParser()
+        cfg_path = os.path.expanduser("~/.clitter")
+        config.read([cfg_path])
+        user = config.get("auth", "username")
+        password = config.get("auth", "password")
+        self.credentials = [user, password]
+
+    def quit(self, status):
+        if status != 0:
+            print usage
+        sys.exit(status)
+
+    def parse_args(self):
+        if len(sys.argv) != 3:
+            self.quit(1)
+        if sys.argv[1] in ["a", "d", "f"]:
+            self.command = sys.argv[1]
+        else:
+            self.quit(1)
+
+    def main(self):
+        self.parse_args()
+        self.read_config()
+        self.command = sys.argv[1]
+        self.handle_command()
+
+    def handle_command(self):
+        if self.command == "a":
+            self.command_add()
+        elif self.command == "f":
+            self.command_fetch()
+        elif self.command == "d":
+            self.command_destroy()
+        else:
+            assert "Unknown command %s" % self.command
+
+    def command_destroy(self):
+        status_id = sys.argv[2]
+        api = twitter.APIRequest(self.credentials[0], self.credentials[1])
+        json = api.destroy(status_id)
+        if json.has_key("id"):
+            print "Destroyed status %d" % json["id"]
+        else:
+            print "Failed to destroy status %d, response was:" % int(status_id)
+            pprint(json)
+
+    def command_fetch(self):
+        pass
+
+    def command_add(self):
+        status = sys.argv[2]
+        api = twitter.APIRequest(self.credentials[0], self.credentials[1])
+        json = api.update(status)
+        if json.has_key("id"):
+            print "Updated your status, id is %d" % json["id"]
+        else:
+            print "Failed to update your status, response was:"
+            pprint(json)
+
+
+
+if __name__ == '__main__':
+    app = Clitter()
+    app.main()
---
+++ twitter/__init__.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2008, Konstantin Merenkov <kmerenkov@gmail.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY Konstantin Merenkov <kmerenkov@gmail.com> ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <copyright holder> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+import urllib
+import urllib2
+import cjson
+
+twitter_url_prefix = 'http://twitter.com/statuses/'
+
+
+def request_get(url):
+    try:
+        f = urllib2.urlopen(url)
+        retval = f.read()
+        f.close()
+    except urllib2.HTTPError, e:
+        print e
+        retval = "{}"
+    return retval
+
+def request_post(url, username, password, data):
+    auth_handler = urllib2.HTTPBasicAuthHandler()
+    auth_handler.add_password(realm='Twitter API',
+                              uri=url,
+                              user=username,
+                              passwd=password)
+    opener = urllib2.build_opener(auth_handler)
+    try:
+        f = opener.open(url, data)
+        retval = f.read()
+        f.close()
+    except urllib2.HTTPError, e:
+        print e
+        retval = "{}"
+    return retval
+
+
+class NotAuthorizedError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return self.value
+
+
+class APIRequest(object):
+    def __init__(self, username='', password=''):
+        self.username = username
+        self.password = password
+
+    def get_public_timeline(self):
+        url = "%s%s" % (twitter_url_prefix, "public_timeline.json")
+        got_data = request_get(url)
+        json = cjson.decode(got_data)
+        return json
+
+    def get_friends_timeline(self, since=None, since_id=None, count=None, page=None):
+        """
+        Returns the 20 most recent statuses posted by the authenticating user
+        and that user's friends. This is the equivalent of /home on the Web.
+        """
+        if not all([self.username, self.password]):
+            raise NotAuthorizedError("Both username and password required")
+        url = "%s%s" % (twitter_url_prefix, "friends_timeline.json")
+        got_data = request_get(url)
+        json = cjson.decode(got_data)
+        return json
+
+    def update(self, status, in_reply_to_status_id=None):
+        """
+        Updates the authenticating user's status. Requires the status parameter
+        specified below.  Request must be a POST.
+        """
+        if not all([self.username, self.password]):
+            raise NotAuthorizedError("Both username and password required")
+        url = "%s%s" % (twitter_url_prefix, "update.json")
+        status = "status=%s" % urllib.quote(status)
+        got_data = request_post(url, self.username, self.password, status)
+        json = cjson.decode(got_data)
+        return json
+
+    def destroy(self, id):
+        """
+        Destroys the status specified by the required ID parameter.
+        The authenticating user must be the author of the specified status.
+        """
+        if not all([self.username, self.password]):
+            raise NotAuthorizedError("Both username and password required")
+        url = "%s%s" % (twitter_url_prefix, "destroy/%s.json" % id)
+        got_data = request_post(url, self.username, self.password, "")
+        json = cjson.decode(got_data)
+        return json
+
+    def get_user_timeline(self, user_id=None, count=None, since=None, since_id=None, page=None):
+        """
+        Returns the 20 most recent statuses posted from the authenticating user.
+        It's also possible to request another user's timeline via the id
+        parameter below. This is the equivalent of the Web /archive page for
+        your own user, or the profile page for a third party.
+        """
+        if not all([self.username, self.password]):
+            raise NotAuthorizedError("Both username and password required")
+        url = "%s%s" % (twitter_url_prefix, "user_timeline.json")
+        if user_id is not None:
+            user_id = "id=%s" % user_id
+        else:
+            user_id = "id=%s" % self.username
+        got_data = request_get("%s/%s" % (url, user_id))
+        json = cjson.decode(got_data)
+        return json
